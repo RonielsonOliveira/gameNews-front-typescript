4e0f811f63d86c879db77b49c4dc88245c4641d1
vboxuser <ronielsonoliveira@live.com>
2026-02-25 13:46:02 -0300

    layout changes

--- src/pages/categories/[category].tsx
+++ src/pages/categories/[category].tsx
@@ -1,7 +1,7 @@
 import HomePage from "@/containers/HomePage";
 import { getAllPosts } from "@/data/posts/get-all-posts";
 import { PostData } from "@/domain/posts/post";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
 export type CategoryProps = {
   posts: PostData[];
   category: string;
@@ -10,17 +10,29 @@ export default function Category({ posts, category }: CategoryProps) {
   return <HomePage posts={posts} category={category} />;
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const category = Array.isArray(ctx.query.category)
-    ? ctx.query.category[0]
-    : ctx.query.category;
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const category = params?.category as string;
+
   const urlQuery = `sort=id:desc
   &pagination[start]=0
   &pagination[limit]=30
-  &filters[category][name][$contains]=${encodeURIComponent(category ?? "")}`;
+  &filters[category][name][$contains]=${encodeURIComponent(category)}`.replace(
+    /\s/g,
+    "",
+  );
   const posts = await getAllPosts(urlQuery);
   console.log(posts);
   return {
-    props: { posts, category: ctx.query.category },
+    props: { posts, category },
+  };
+};
+export const getStaticPaths: GetStaticPaths = async () => {
+  const categories = ["pc", "playstation", "xbox", "nintendo"];
+
+  return {
+    paths: categories.map((category) => ({
+      params: { category },
+    })),
+    fallback: "blocking",
   };
 };
